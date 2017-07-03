02db64dfc4b15f634e24163207285d42135a5f99
IFoxH <oldsuw@gmail.com>
2017-07-03 18:47:22 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -62,7 +62,7 @@ function setCookie(c_name,value,exporedays){
   s += c_name + escape(value);
   s += "; path="+ location.pathname;
   if(expiredays){
-    s += "; expires=" +exdate+; ";
+    s += "; expires=" +exdate+"; ";
   }else{
     s += "; ";
   }
